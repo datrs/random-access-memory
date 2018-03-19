a48c45db74eb8cf943a0e1daa2e3f3a124867a7c
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-20 07:50:04 +1100

    api outline

--- src/sync.rs
+++ src/sync.rs
@@ -1,31 +1,61 @@
-/// A synchronous implementation of `random_access_memory`.
-pub struct Sync {
-  /// The length length of each buffer.
-  pub page_size: usize,
+extern crate random_access_storage as random_access;
 
-  /// Memory we read/write to.
-  pub buffers: Vec<Vec<u8>>,
+use self::random_access::Error;
 
-  /// Total length of the data.
-  pub length: u64,
-}
-
+/// Main constructor.
+pub struct Sync;
 impl Sync {
-  /// Create a new `Sync` instance.
-  pub fn new() -> Self {
-    Sync {
+  /// Create a new instance.
+  pub fn new() -> random_access::Sync<SyncMethods> {
+    let methods = SyncMethods {
       page_size: 1024 * 1024,
       buffers: Vec::new(),
       length: 0,
-    }
+    };
+
+    random_access::Sync::new(methods)
+  }
+
+  /// Create a new instance, but pass the initial buffers to the constructor.
+  pub fn with_buffers(
+    buffers: Vec<Vec<u8>>,
+  ) -> random_access::Sync<SyncMethods> {
+    let methods = SyncMethods {
+      page_size: 1024 * 1024,
+      buffers: buffers,
+      length: 0,
+    };
+
+    random_access::Sync::new(methods)
+  }
+}
+
+/// Methods that have been implemented to provide synchronous access to memory
+/// buffers. These should generally be kept private, but exposed to prevent
+/// leaking internals.
+pub struct SyncMethods {
+  /// The length length of each buffer.
+  pub page_size: usize,
+
+  /// The memory we read/write to.
+  pub buffers: Vec<Vec<u8>>,
+
+  /// Total length of the data.
+  length: u64,
+}
+
+impl random_access::SyncMethods for SyncMethods {
+  /// Open the instance (noop).
+  fn open(&self) -> Result<(), Error> {
+    Ok(())
   }
 
   /// Write bytes to memory.
-  // pub fn write(offset: u64, data: Vec<u8>) {}
+  fn write(&self, offset: u64, data: Vec<u8>) -> Result<(), Error> {}
 
   /// Read bytes from memory.
-  pub fn read() {}
+  fn read(&self, offset: u64, length: u64) -> Result<Vec<u8>, Error> {}
 
   /// Delete bytes from memory.
-  pub fn del() {}
+  fn del(&self, offset: u64, length: u64) -> Result<(), Error> {}
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,6 +1,17 @@
-extern crate random_access_memory as ram;
+mod sync {
+  extern crate random_access_memory as ram;
 
-#[test]
-fn can_call_new() {
-  let _file = ram::Sync::new();
+  #[test]
+  fn can_call_new() {
+    let _file = ram::Sync::new();
+  }
+
+  #[test]
+  fn can_open_buffer_noop() {
+    let file = ram::Sync::new();
+    assert!(file.open().is_ok());
+  }
+
+  #[test]
+  fn can_write() {}
 }
