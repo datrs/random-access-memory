a1d09362b8ada7e9b9a044e7033a32bb31ef5305
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-23 12:05:05 +1100

    add benchmarks

---
+++ benches/sync.rs
@@ -0,0 +1,37 @@
+#![feature(test)]
+
+mod sync {
+  extern crate random_access_memory as ram;
+  extern crate test;
+
+  use self::test::Bencher;
+
+  #[bench]
+  fn write_hello_world(b: &mut Bencher) {
+    b.iter(|| {
+      let mut file = ram::Sync::new();
+      file.write(0, b"hello").unwrap();
+      file.write(5, b" world").unwrap();
+    });
+  }
+
+  #[bench]
+  fn read_hello_world(b: &mut Bencher) {
+    let mut file = ram::Sync::new();
+    file.write(0, b"hello").unwrap();
+    file.write(5, b" world").unwrap();
+    b.iter(|| {
+      let _text = file.read(0, 11).unwrap();
+    });
+  }
+
+  #[bench]
+  fn read_write_hello_world(b: &mut Bencher) {
+    b.iter(|| {
+      let mut file = ram::Sync::new();
+      file.write(0, b"hello").unwrap();
+      file.write(5, b" world").unwrap();
+      let _text = file.read(0, 11).unwrap();
+    });
+  }
+}
