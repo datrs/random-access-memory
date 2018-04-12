fe44a5111166d43c302a786a05c9aeed20c3d294
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-12 21:13:32 +0200

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,6 @@
 #![cfg_attr(test, feature(plugin))]
 #![cfg_attr(test, plugin(clippy))]
 
-//! ## Usage
-//! ```rust,ignore
-//! extern crate random_access_memory as ram;
-//!
-//! let mut file = ram::Sync::new();
-//! file.write(0, b"hello")?;
-//! file.write(5, b" world")?;
-//! let text = file.read(0, 11)?;
-//! assert_eq!(text, b"hello world");
-//! ```
-
 #[macro_use]
 extern crate failure;
 
